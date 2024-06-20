4c62b409d8dd143ace0d95433d44e0cf6a5511f5
Smalick0478 <sameermalik0478@gmail.com>
2024-06-20 11:42:11 +0500

    Single Job route added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Jobs\JobsController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Auth::routes();
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/jobs/single/{id}', [JobsController::class, 'single'])->name('single.job');
