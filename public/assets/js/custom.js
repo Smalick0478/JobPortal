f6233aa95db67a0e1c633717fc6df7bbfe818603
Smalick0478 <sameermalik0478@gmail.com>
2024-06-20 11:43:31 +0500

    homepage animation reduce

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -223,7 +223,7 @@ jQuery(function ($) {
                                     number: num,
                                     numberStep: comma_separator_number_step,
                                 },
-                                7000
+                                2000
                             );
                         });
                     }
